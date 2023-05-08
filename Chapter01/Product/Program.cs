a5ce513ca3b5898bcf63f1923c7d031c2a171f57
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-08 11:44:18 +0900

    0508演習

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -9,13 +9,27 @@ namespace ProductSample {
     
     class Program {
         static void Main(string[] args) {
+
+
             //インスタンスの生成
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daihuku = new Product(235, "大福もち", 160);
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daihuku = new Product(235, "大福もち", 160);
 
-            Console.WriteLine("かりんとうの税込み価格"+karinto.GetPriceIncludingTax());
-            Console.WriteLine("大福もちの税込み価格"+daihuku.GetPriceIncludingTax());
+            //Console.WriteLine("かりんとうの税込み価格"+karinto.GetPriceIncludingTax());
+            //Console.WriteLine("大福もちの税込み価格"+daihuku.GetPriceIncludingTax());
 
+            DateTime date = new DateTime(2023,5,8);
+
+
+            Console.WriteLine("今日の日付:" + date.Year + "年" + date.Month + "月" + date.Day + "日");
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("10日後:" + daysAfter10.Year+"年"+daysAfter10.Month+"月"+daysAfter10.Day+"日");
+
+            //10日前
+            DateTime daysBefore10 = date.AddDays(-10);
+            Console.WriteLine("10日前:" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日");
         }
     }
 }
