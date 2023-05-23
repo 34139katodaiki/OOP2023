97fd508c216816453e59966ae7e9adbd76be3d86
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-23 10:30:38 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -9,10 +9,11 @@ namespace Section01 {
 
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, 4, 2, 6, 8, 5, 13, 15 };
 
-            int count = numbers.Count(n => n % 2 == 0);
-            Console.WriteLine(count);
+            var sum = numbers.Where(n => n % 2 == 0).Sum();
+            //int count = numbers.Count(n => n % 5 == 0);
+            Console.WriteLine(sum);
         }
 
 
