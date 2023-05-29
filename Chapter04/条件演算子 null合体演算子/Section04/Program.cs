67fb4afd38a876d317230fa9149d4d716a16ef0f
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-29 10:53:55 +0900

    条件演算子

---
+++ Chapter04/条件演算子 null合体演算子/Section04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+
+
+            var list = new List<int> { 10, 20, 30, 40 };
+            var key = 40;
+
+            var num = list.Contains(key) ? 1 : 0;   //条件演算子・参考演算子
+            Console.WriteLine(num);
+        }
+    }
+}
