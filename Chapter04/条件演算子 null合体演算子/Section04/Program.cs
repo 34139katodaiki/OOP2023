46d0d332f21ee7c8743611fe38865097ab984109
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-29 11:06:59 +0900

    null合体演算子

--- Chapter04/条件演算子 null合体演算子/Section04/Program.cs
+++ Chapter04/条件演算子 null合体演算子/Section04/Program.cs
@@ -8,12 +8,27 @@ namespace Section04 {
     class Program {
         static void Main(string[] args) {
 
+            #region 条件演算子
+            //var list = new List<int> { 10, 20, 30, 40 };
+            //var key = 40;
 
-            var list = new List<int> { 10, 20, 30, 40 };
-            var key = 40;
+            //var num = list.Contains(key) ? 1 : 0;   //条件演算子・参考演算子
+            //Console.WriteLine(num);
+            #endregion
 
-            var num = list.Contains(key) ? 1 : 0;   //条件演算子・参考演算子
-            Console.WriteLine(num);
+            #region null合体演算子
+            string code = "12345";
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+            #endregion
+        }
+
+        private static object DefaultMessage() {
+            return "Default Message";
+        }
+
+        private static object GetMessage(object code) {
+            return null;
         }
     }
 }
