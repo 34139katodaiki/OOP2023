b9c771b8db48dc7045ddc3e9439ccd4805e48d95
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-30 11:24:05 +0900

    4.1.2

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+            
+        }
+
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+
+        //4.1.3
+        //public YearMonth AddOneMonth() {
+            
+        //}
+
+        //4.1.4
+        //public override string ToString() {
+        //    
+        //}
+
+
+    }
+    
+}
+
+
