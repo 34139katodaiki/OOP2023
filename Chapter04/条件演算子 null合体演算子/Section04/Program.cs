6e2de17e01976116561033b73bf69394dc02e023
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-30 10:01:18 +0900

    TryParseまで完成

--- Chapter04/条件演算子 null合体演算子/Section04/Program.cs
+++ Chapter04/条件演算子 null合体演算子/Section04/Program.cs
@@ -17,11 +17,28 @@ namespace Section04 {
             #endregion
 
             #region null合体演算子
-            string code = "12345";
-            var message = GetMessage(code) ?? DefaultMessage();
-            Console.WriteLine(message);
+            //string code = "12345";
+            //var message = GetMessage(code) ?? DefaultMessage();
+            //Console.WriteLine(message);
             #endregion
+            #region null条件演算子
+
+            //Sale sale = new Sale {
+            //    Amount = 100
+            //};
+
+
+            Sale sale = null;
+                
+            //int?はnull許容型、「?.」はnull条件演算子
+            int? ret = sale?.Amount;
+
+            Console.WriteLine(ret);
+            #endregion
+
         }
+
+
 
         private static object DefaultMessage() {
             return "Default Message";
@@ -30,5 +47,14 @@ namespace Section04 {
         private static object GetMessage(object code) {
             return null;
         }
+    }
+
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //商品カテゴリー
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
     }
 }
