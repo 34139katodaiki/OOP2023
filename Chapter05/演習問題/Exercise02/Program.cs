aa772eb50dca2809eb49190297d78bfeb96df53e
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-31 11:55:41 +0900

    5.2

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("数字列:");
+            var line = Console.ReadLine();
+            int num;
+            if(int.TryParse(line,out num)) {
+                var s1 = string.Format("{0,10:#,0}", num);
+                Console.WriteLine(s1);
+            }
+        }
+    }
+}
