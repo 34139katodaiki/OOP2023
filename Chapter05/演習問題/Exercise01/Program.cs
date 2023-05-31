d6bd6df8f29d235e7e5189a0826978c2d2a3d3dd
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-05-31 11:42:18 +0900

    5.1

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("文字列1:");
+            var s1 = Console.ReadLine();
+
+            Console.WriteLine("文字列2:");
+            var s2 = Console.ReadLine();
+
+            if (String.Compare(s1, s2, ignoreCase: true) == 0) {
+                Console.WriteLine("等しい");
+            }
+            else {
+                Console.WriteLine("等しくない");
+            }
+
+            
+        }
+    }
+}
