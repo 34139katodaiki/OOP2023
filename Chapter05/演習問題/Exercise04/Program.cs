0dfaf8600d9cdbc284f453495b23afee6e4539b8
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-06-06 11:13:46 +0900

    5.4模範

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,7 +1,8 @@
-﻿#define Nonarray
-//#define Stringarray
+﻿//#define Nonarray
+#define Stringarray
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +10,51 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 #if Nonarray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            
 
             var s = line.Split(';');
-            var loveli = s[0].Split('=');
-            var book = s[1].Split('=');
-            var born = s[2].Split('=');
-            Console.WriteLine("作者   : "+loveli[1]);
-            Console.WriteLine("代表作 : "+book[1]);
-            Console.WriteLine("誕生年 : "+born[1]);
+
+            foreach (var pair in line.Split(';')) {
+                var array = pair.Split('=');
+                Console.WriteLine("{0}:{1}", Tojapanese(array[0]),array[1]);
+
+            }
+
+
+            
 #elif Stringarray
             string[] lines = {
-
+            "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+            "Novelist=太宰治;BestWork=人間失格;Born=1909",
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896"
             };
+            foreach (var line in lines) {
+                foreach (var pair in line.Split(';')) {
+                    var array = pair.Split('=');
+                    Console.WriteLine("{0}:{1}", Tojapanese(array[0]), array[1]);
+
+                }
+            }
+            Console.WriteLine("実行時間:{0}",sw.Elapsed.ToString(@"ss\.fffff"));
 #endif
 
         }
 
+        static string Tojapanese(string key) {
+            switch (key) {
+                case "Novelist":
+                    return "作家 ";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+            }
+            throw new ArgumentException("正しい引数でありません");
+        }
     }
 }
