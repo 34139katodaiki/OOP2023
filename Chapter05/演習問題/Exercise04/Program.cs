69866039e200232bd7b33da33bbfbc5254fb757d
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-06-06 09:45:12 +0900

    5.4

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define Nonarray
+//#define Stringarray
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +9,24 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+
+#if Nonarray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 
+            var s = line.Split(';');
+            var loveli = s[0].Split('=');
+            var book = s[1].Split('=');
+            var born = s[2].Split('=');
+            Console.WriteLine("作者   : "+loveli[1]);
+            Console.WriteLine("代表作 : "+book[1]);
+            Console.WriteLine("誕生年 : "+born[1]);
+#elif Stringarray
+            string[] lines = {
+
+            };
+#endif
+
         }
+
     }
 }
