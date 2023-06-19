003d9f74e05c6913f4d5238e35a43f1b7760f193
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-06-19 15:29:08 +0900

    設問1_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,17 +7,30 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
 
 
-            
+
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要： 点数データを読み込み、studentsオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            var students = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
 
+            foreach (var line in lines) {
+                var person = line.Split(',');
+
+                var student = new Student {
+                    Name = person[0],
+                    Subject = person[1],
+                    Score = int.Parse(person[2])
+                };
+                students.Add(student);
+            }
+            return students;
 
 
 
@@ -26,9 +39,19 @@ namespace Test01 {
             
         }
 
-        //メソッドの概要： 
+        //メソッドの概要： 科目別の点数合計を求める
         public IDictionary<string, int> GetPerStudentScore() {
-
+            var dict = new SortedDictionary<string, int>();
+            foreach (var student in _score) {
+                if (dict.ContainsKey(student.Subject)) {
+                    dict[student.Subject] += student.Score;
+                }
+                else {
+                    dict[student.Subject] = student.Score;
+                }
+                
+            }
+            return dict;
 
 
 
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,5 +1,5 @@
 ﻿namespace Test01 {
-    class Student {
+    public class Student {
         public string Name { get; set; }    //学生の名前
         public string Subject { get; set; } //科目名
         public int Score { get; set; }      //点数
