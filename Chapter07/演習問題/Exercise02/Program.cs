851c53554646c84f0745a836c1df73a30b82c643
加藤大暉 <OIBC34139@ojs.ac.jp>
2023-06-29 11:41:05 +0900

    7.2.1

---
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var abbrs = new Abbreviations();
+            abbrs.Count();
+        }
+    }
+}
