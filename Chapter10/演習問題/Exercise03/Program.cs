c982766ccd88235ed89cb1082efc24636a0ca7d6
34139katodaiki <OIBC34139@ojs.ac.jp>
2023-10-05 10:09:33 +0900

    Exercise03

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+            foreach (var text in texts) {
+                var regex = Regex.Matches(text, @"\btime\b", RegexOptions.IgnoreCase);
+                foreach (Match match in regex) {
+                    Console.WriteLine("{0}: {1}",text,match.Index);
+                }
+            }
+            
+
+
+
+        }
+    }
+}
