a64f2239320be2088f30da7fd41c28259ba875f6
34139katodaiki <OIBC34139@ojs.ac.jp>
2023-10-05 09:57:56 +0900

    Exercise02

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Pickup3DigitNumber("sample.txt");
+        }
+
+        private static void Pickup3DigitNumber(string file) {
+            foreach (var line in File.ReadLines(file)) {
+                var match = Regex.Matches(line, @"\b(\d{3,})\b");
+                foreach (Match match1 in match) {
+                    Console.WriteLine(match1.Value);
+                }
+
+
+            }
+        }
+    }
+}
