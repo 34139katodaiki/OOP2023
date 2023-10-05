633d01f09a43b22f152641e0e508b13321b066dd
34139katodaiki <OIBC34139@ojs.ac.jp>
2023-10-05 10:58:33 +0900

    Exercise04

---
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+            var filename = "sample.txt";
+            using (StreamWriter sw = new StreamWriter(filename, false, Encoding.GetEncoding("Shift_JIS"))) {
+                foreach (var line in lines) {
+                    var replaced = Regex.Replace(line, @"(V|v)ersion=""v4.0""", @"version=""v5.0""");
+                    sw.WriteLine(replaced);
+                }
+            }
+            
+
+
+
+
+
+            // これ以降は確認用
+            var text = File.ReadAllText("sample.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
