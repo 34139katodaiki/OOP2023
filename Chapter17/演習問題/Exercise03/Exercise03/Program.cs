2433cf4469ad15aa2cd98d9a4e11215cadecca2b
34139katodaiki <OIBC34139@ojs.ac.jp>
2023-10-25 11:45:20 +0900

    GridLayoutのサンプル

---
+++ Chapter17/演習問題/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            //TextFileProcessor.Run(@"C:\Users\infosys\source\repos\OOP2023\Chapter17\演習問題\Exercise01\Sample.txt");
+        }
+    }
+}
---
+++ Chapter17/演習問題/Exercise03/Exercise03/ToHankakuProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class ToHankakuProcessor : ITextFileService {
+
+        private static Dictionary<char, char> _dictionary = new Dictionary<char, char>() {
+            {'０','0'},{'１','1'},{'２','2'},{'３','3'},{'４','4'},{'５','5'},{'６','6'},{'７','7'},{'８','8'},{'９','9'},
+        };
+
+        public void Initialize(string fname) {
+            
+        }
+
+        public void Terminate() {
+            
+        }
+
+
+        void ITextFileService.Execute(string line) {
+            var rep = Regex.Replace(line, "[０-９]", c => _dictionary[c.Value[0]].ToString());
+            Console.WriteLine(rep);
+        }
+    }
+}
